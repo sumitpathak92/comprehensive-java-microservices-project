f3b6d865b31395f99b6f77e3740815bddaf04703
Sumit Pathak <pathaksumit92@gmail.com>
2025-01-16 17:59:14 +0530

    adds notification service changes

--- order-service/src/main/java/com/sumit/microservices/order/dto/OrderRequest.java
+++ order-service/src/main/java/com/sumit/microservices/order/dto/OrderRequest.java
@@ -3,5 +3,8 @@ package com.sumit.microservices.order.dto;
 import java.math.BigDecimal;
 
 public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price,
-                           Integer quantity) {
+                           Integer quantity, UserDetails userDetails) {
+
+    public record UserDetails(String email, String firstName, String lastName) {}
+
 }
